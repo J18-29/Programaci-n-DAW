9550918b1071b7eb98915bbffa286ac87d5a7fa9
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-03 09:57:44 +0200

    Actividades

--- actividades/src/Actividad3.java
+++ actividades/src/Actividad3.java
@@ -2,33 +2,39 @@ import java.util.Scanner;
 
 public class Actividad3 {
     public static void main(String[] args) {
-         //Desarrolla un programa que muestre la edad máxima y mínima de un grupo de alumnos que se introduzca hasta escribir -1
-         int maximo=1;
-         int minimo=1000000;
-         int contadorPersona=0;
+        // Desarrolla un programa que muestre la edad máxima y mínima de un grupo de alumnos
+        // que se introduzca hasta escribir -1
+        int maximo = Integer.MIN_VALUE;
+        int minimo = Integer.MAX_VALUE;
+        int contadorPersona = 0;
 
         Scanner teclado = new Scanner(System.in);
-       int edad;
-         do {
+        int edad;
+
+        do {
             System.out.println("Introduce una edad o -1 para salir: ");
-            edad=teclado.nextInt();
-           if(edad!=-1) contadorPersona++;//Nueva persona
-            //Establezco la logica para maximo y minimo
-            if(maximo>edad){
-                maximo=edad;
+            edad = teclado.nextInt();
+
+            if (edad != -1) {
+                contadorPersona++; // Nueva persona
+
+                // Establezco la lógica para máximo y mínimo
+                if (edad > maximo) {
+                    maximo = edad;
+                }
+                if (edad < minimo) {
+                    minimo = edad;
+                }
             }
-            if(minimo<edad);{
-                minimo=edad;
-            }
-         } while (edad!=-1);
-         System.out.println("El numero de personas"+contadorPersona);
-         System.out.println("El maximo es:"+maximo);
-         System.out.println("El minimo es:"+minimo);
-         
-         
-         
-         
+        } while (edad != -1);
+
+        if (contadorPersona > 0) {
+            System.out.println("El número de personas: " + contadorPersona);
+            System.out.println("La edad máxima es: " + maximo);
+            System.out.println("La edad mínima es: " + minimo);
+        } else {
+            System.out.println("No se introdujo ninguna edad válida.");
         }
+    }
+}
 
-
- }
