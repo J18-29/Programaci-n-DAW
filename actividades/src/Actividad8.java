34711053f954338ae4a97a4843f40914327f63f0
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-06 13:00:20 +0200

    Actividades

---
+++ actividades/src/Actividad8.java
@@ -0,0 +1,16 @@
+public class Actividad8 {
+    public static void main(String[] args) {
+        //Programa que muestra las tablas de multiplicar del 1 al 10
+        for (int i=1; i<=10;i++){
+
+            for(int j=1;j<=10;j++){
+
+                System.out.println(i+"*"+j+"= "+(i*j));
+
+            }
+              System.out.println(); 
+        }
+
+       
+    }
+}
