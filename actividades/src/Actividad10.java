cb1bf32fcf54eb20fc7139426e551ed4a1f6eaa4
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-07 09:31:14 +0200

    Ejercicios

--- actividades/src/Actividad10.java
+++ actividades/src/Actividad10.java
@@ -1,6 +1,22 @@
 public class Actividad10 {
+
+    public static boolean esPar(int n) {
+        return n % 2 == 0;
+    }
+
+    public static boolean esDivisible3(int n) {
+        return n % 3 == 0;
+    }
+
+    public static void imprimirParesDiv3() {
+        for (int i = 1; i <= 20; i++) {
+            if (esPar(i) && esDivisible3(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        //Funciones y Procedimiento
-        
+        imprimirParesDiv3();
     }
 }
--- actividades/src/Actividad9.java
+++ actividades/src/Actividad9.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 
 public class Actividad9 {
-    final static double PI=3.14159;
-    public static void main(String[] args) {
-    //calcular el área y volumen de un cilindro (2*PI*(altura+radio), PI*radio2*altura)
-     Scanner teclado=new Scanner(System.in);
-     double altura;
-     double radio;
-     //leer parametros
+    final static double PI = 3.14159;
 
-     System.out.println("Escribe la altura y radio del cilindro: ");
-     altura= teclado.nextDouble();
-     radio=teclado.nextDouble();
-     imprimeCilindro(altura, radio);
+    public static void main(String[] args) {
+        // Calcular el área y volumen de un cilindro: 
+        // Área = 2 * PI * r * (r + h)
+        // Volumen = PI * r^2 * h
+
+        Scanner teclado = new Scanner(System.in);
+        double altura;
+        double radio;
+
+        // Leer parámetros
+        System.out.println("Escribe la altura y el radio del cilindro: ");
+        altura = teclado.nextDouble();
+        radio = teclado.nextDouble();
+
+        imprimeCilindro(altura, radio);
     }
-    public static void imprimeCilindro(double altura,double radio){
-        System.out.println("El area del cilindro es: "+(altura*radio*PI));
-          System.out.println("El VOLUMEN del cilindro es: "+(altura*radio*PI));
-     }
+
+    public static void imprimeCilindro(double altura, double radio) {
+        double area = 2 * PI * radio * (radio + altura);
+        double volumen = PI * radio * radio * altura;
+
+        System.out.println("El área del cilindro es: " + area);
+        System.out.println("El volumen del cilindro es: " + volumen);
+    }
 }
