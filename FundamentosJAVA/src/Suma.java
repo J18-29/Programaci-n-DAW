da6caec51d93c5c3d7154fcaff1b1e5ef867c918
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-08 14:30:02 +0200

    Auh

--- FundamentosJAVA/src/Suma.java
+++ FundamentosJAVA/src/Suma.java
@@ -1,8 +1,9 @@
 public class Suma {
    public static void incre(int n){
  if(n>0){
- incre(n-1);
- System.out.print(n);
+System.out.print(n);
+incre(n-1);
+ 
  }
  else
  System.out.println();
---
+++ actividades/src/Decimal.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Decimal {
+   //Método recursivo para pasar un número decimal, que es positivo, y pasarlo a binario mediantesucesivas divisiones por 2
+     public static int pasarBinario(int n){
+        String cadena="";
+        if(n==0){
+            return cadena;
+        }
+         else{
+            cadena=cadena+n%2;
+            pasarBinario(n/2);
+         }
+        return 1;
+    }
+
+
+
+}
