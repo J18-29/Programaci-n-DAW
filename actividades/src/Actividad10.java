388f7bd04fd64d1742cf08475ec0c8106474600a
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-10 13:51:46 +0200

    Corregcion de ejercicios

--- actividades/src/Actividad10.java
+++ actividades/src/Actividad10.java
@@ -1,11 +1,11 @@
 
 
 public class Actividad10 {
-
+    //Defnimos los numeros pares
     public static boolean esPar(int n) {
         return n % 2 == 0;
     }
-
+    //Definimos los numeros divisibles por 3
     public static boolean esDivisible3(int n) {
         return n % 3 == 0;
     }
