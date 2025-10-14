3514c037f5f6ad2d4ad627ca7e53fba01e105107
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-14 13:27:27 +0200

    correcion ejercicios

---
+++ FundamentosJAVA/src/Practica.java
@@ -0,0 +1,16 @@
+public class Practica {
+    //PRÁCTICA RECORRER MATRICES
+        
+        int[][] matriz2={{1,2,3,4,5},{2,3,4,5},{3,4,5},{4,5},{5}}; 
+
+
+for (int i = 0; i < matriz.length; i++) {//RECORRE las filas
+    for (int j = 0; j < matriz2[i].length; j++){
+ System.out.print(matriz2[i][j] + " ");
+ }
+ System.out.println();
+}
+
+}
+    
+
