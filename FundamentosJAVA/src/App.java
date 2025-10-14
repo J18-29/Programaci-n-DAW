77c573d42b227ed4736898857db8bfafd05b45e4
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-14 12:32:20 +0200

    Arrays

--- FundamentosJAVA/src/App.java
+++ FundamentosJAVA/src/App.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class App {
+  public static double devolverArrray(double[]) {
+    
+  }
     public static void main(String[]arg) throws Exception{
     String nombre1;
     String Apellidos1;
@@ -21,5 +24,28 @@ public class App {
   System.out.println(calificaciones2[0]);
   calificaciones=null;
   System.out.println(calificaciones[0]);
+
+//Arrays que apuntan al mismo sitio
+double[] calificacionesOtro;
+calificacionesOtro=calificaciones;
+System.out.println(calificaciones[3]);
+System.out.println(calificacionesOtro[3]);
+calificaciones=null;
+System.out.println(calificacionesOtro[3]);
+
+//Imprime la posicion 1
+imprimeArray(calificaciones);
+double[] resultado=devolverArrray(calificaciones);
+System.out.println(resultado[1]);
+
+//Usar la memoria dinamica reservada
+calificaciones2[3]=5;
+System.out.println(calificaciones2[3]);
+System.out.println(calificaciones2[1]);
   }
+
+    private static void imprimeArray(double[] calificaciones) {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'imprimeArray'");
+    }
 }
