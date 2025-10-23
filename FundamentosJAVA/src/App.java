3bedccfc15773dda93022cf9e32a66528f0161b8
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-23 08:59:22 +0200

    App.java

--- FundamentosJAVA/src/App.java
+++ FundamentosJAVA/src/App.java
@@ -1,56 +1,27 @@
-import java.util.Scanner;
-
 public class App {
-  public static double devolverArrray(double[]) {
-    
-  }
-    /**
-     * @param arg
-     * @throws Exception
-     */
-    public static void main(String[]arg) throws Exception{
-    String nombre1;
-    String Apellidos1;
-    int edad1;
-    double notaBD1;
-    double notaPROG1;
+public static void main(String[] args) {
+  char[] cadena1={'J','o','s','e'};
+  char[] cadena2={'J','o','s','e'};
+  if (compararCadenas(cadena1, cadena2)) System.out.println("Son iguales"); 
+   else System.out.println("Son diferentes"); 
+  
 
-    double[] calificaciones={7,8,2,4,8,9};
+   String palabra1="Jose";
+   String palabra2="Jose";
+   if(palabra1.equals(palabra2)) System.out.println("Son iguales");
+   else System.out.println("Son diferentes");
 
-    double calificaciones2[];
-
-   System.out.println(calificaciones[0]);
-
-  //  System.out.println("Introduce el tamaño del array");
-  //  Scanner teclado= new Scanner(System.in);
-  //  int numero=teclado.nextInt();
-  calificaciones2=calificaciones;
-  System.out.println(calificaciones2[0]);
-  calificaciones=null;
-  System.out.println(calificaciones[0]);
-
-//Arrays que apuntan al mismo sitio
-double[] calificacionesOtro;
-calificacionesOtro=calificaciones;
-System.out.println(calificaciones[3]);
-System.out.println(calificacionesOtro[3]);
-calificaciones=null;
-System.out.println(calificacionesOtro[3]);
-
-//Imprime la posicion 1
-imprimeArray(calificaciones);
-double[] resultado=devolverArrray(calificaciones);
-System.out.println(resultado[1]);
-
-//Usar la memoria dinamica reservada
-calificaciones2[3]=5;
-System.out.println(calificaciones2[3]);
-System.out.println(calificaciones2[1]);
-
-int[][] matriz={{1,2},{3,4}}; 
-for(int i=0;i<2;i++){ 
-  for (int j=0;j<2;j++) System.out.print(matriz[i][j]+" "); 
-  System.out.println(); } 
-  }
- 
+   int[] numeros={3,5,7,9,1,3,54};
+   for(int i=0;i<numeros.length;i++){
+    System.out.println(numeros[i]+"");
+   }
 }
+public static boolean compararCadenas(char[] cadena1,char[] cadena2){
+      if(cadena1.length!=cadena2.length) return false;
+      for(int i=0;i<cadena1.length;i++){
+        if(cadena1[i]!=cadena2[i]) return false;
+      }
+      return true;
+}
+}
+ 
