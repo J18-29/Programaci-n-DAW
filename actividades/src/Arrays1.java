9565d8956b34a7e10b4aacdf4a11138a3c7bf402
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-24 14:20:21 +0200

    Arrays

---
+++ actividades/src/Arrays1.java
@@ -0,0 +1,40 @@
+public class Arrays1 {
+    public static void main(String[] args) {
+        //Crear el vector dinamico
+        int[] vector=null;
+        //Inserta un valor
+        vector=push(vector, 3);
+        //Inserta un valor
+        vector=push(vector, 5);
+         for(int i=0;i<vector.length;i++){
+            System.out.print(vector[i]+"");
+         }
+        //Elimina el último
+        int valor=pop(vector);
+        System.out.println(valor);
+        
+    }
+    public static int[]push(int[] array, int valor){
+        int longitud;
+        if (array==null) {longitud=0;}
+        else {longitud=array.length;}
+           //Crear el array con +1 de tamaño
+           int[] salida=new int[longitud+1];
+           for(int i=0;i<longitud;i++){
+                salida[i]=array[i];
+           }
+           salida[longitud]=valor;
+           return salida;
+    }
+
+    public static int pop(int [] array){
+        int valor=array[array.length-1];
+        int[] nuevo=new int[array.length];
+        //Copiar hasta el tamaño de nuevo
+        for(int i=0;i<nuevo.length;i++){
+            nuevo[i]=array[i];
+        }
+        array=nuevo;
+        return valor;
+    }
+}
