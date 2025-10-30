e6c53552a931e67992d8e58878acf8c812dca03f
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-30 10:48:34 +0100

    Muestra

--- FundamentosJAVA/src/Muestra.java
+++ FundamentosJAVA/src/Muestra.java
@@ -2,7 +2,7 @@ public class Muestra {
     public static void main(String[] args) {
         int array[]={2,5,14,7,8,2,9};
         for(int i=0;i<array.length;i++){
-            System.out.print(array[i]+"");
+            System.out.print(array[i]+""); // El +"" es innecesario, se puede quitar
         }
         System.out.println();
     }
