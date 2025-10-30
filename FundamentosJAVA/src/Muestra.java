d0c82a3a2ef2afcdca261887c329be9713103677
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-10-30 09:00:26 +0100

    Muestra

---
+++ FundamentosJAVA/src/Muestra.java
@@ -0,0 +1,9 @@
+public class Muestra {
+    public static void main(String[] args) {
+        int array[]={2,5,14,7,8,2,9};
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+"");
+        }
+        System.out.println();
+    }
+}
