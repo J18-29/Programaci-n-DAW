146fff3b85922262298b939e57e2b9dffa992b92
J18-29 <josemiguellopezlopez2007@gmail.com>
2025-11-17 14:20:39 +0100

    Actividad de Hoy

--- Clases/src/Prueba/Actividad6Clase.java
+++ Clases/src/Prueba/Actividad6Clase.java
@@ -10,7 +10,7 @@ import clases.Surcursal;
 public class Actividad6Clase{
     public static void main(String[] args) {
         Surcursal sur= new Surcursal(234, "C/menor 34", "Albanchez", "Almeria", "234945618");
-        Revista revis= new Revista(12, "Secretos del mar", "Catalogo", null);
+        Revista revis= new Revista(12, "Secretos del mar", "Catalogo", "20 años");
         Seccion sec= new Seccion("Historia", 20);
         Ejemplar ejem= new Ejemplar("20/12/2000", 45, 10);
         Periodista perio= new Periodista(789, "Paco", "Lopez", "Vega", "761239012", "lector");
