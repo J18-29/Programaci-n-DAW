eb616acbda19c014d15da93a1f8b3794c2dd488b
J18-29 <josemiguellopezlopez2007@gmail.com>
2026-01-13 09:46:26 +0100

    Ajustes java

---
+++ Tema5/src/Heroe.java
@@ -0,0 +1,23 @@
+public class Heroe {
+
+    private String nombre;
+    private String editorial;
+
+    public Heroe(String nombre, String editorial) {
+        this.nombre = nombre;
+        this.editorial = editorial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
